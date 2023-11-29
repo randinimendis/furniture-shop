b07381a605195781440ef404a2be2f036849ca71
Randini Mendis <99355199+randinimendis@users.noreply.github.com>
2023-11-29 21:09:23 +0530

    dev_front_commit

--- project/frontend/src/App.js
+++ project/frontend/src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+//import React, { useState } from 'react';
+//import Register from "./components/UserManagement/register";
+import Home from "./components/Home";
+
+
+//import DeliveryManagament from "./components/DeliveryManagement/DeliveryManagament";
 
 function App() {
+  //const isLoggedIn = localStorage.getItem("loggedIn");
+  //const loggedRole = localStorage.getItem("role");
+
+/*
+  const [code, setCode] = useState('');
+
+  function generateCode() {
+    let code = '';
+    const possibleChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+
+    for (let i = 0; i < 6; i++) {
+      code += possibleChars.charAt(Math.floor(Math.random() * possibleChars.length));
+    }
+
+    return code;
+  }
+
+  function handlePromotionCodeClick() {
+    const newCode = generateCode();
+    setCode(newCode);
+
+  }
+*/
+
+  // const location = useLocation();
+  // console.log(location)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      {/**<Header /> */}
+
+      <Routes>
+      
+       {/** <Route path="/register" element={<Register />} />*/}
+       <Route path="/" element={<Home />} />
+      </Routes>
+
+      {/** <Footer />*/}
+    </BrowserRouter>
   );
 }
 
 export default App;
+
---
+++ project/frontend/src/components/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+//import "../css/home.css";
+//import { Link } from "react-router-dom";
+
+// import Footer from './inc/footer'
+
+function Home() {
+  return (
+    <div> this is home
+     
+    </div>
+  );
+}
+
+export default Home;
